29e668741807337a50ae6c11ea07092569e86bc5
Christo DeLange <github@dldinternet.com>
2015-02-18 02:49:35 -0500

    Vendor chef-server-cluster

---
+++ cookbooks/chef-server-ingredient/test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'chef-server-ingredient::default' do
+  describe package('chef-server-core') do
+    it { should be_installed }
+  end
+
+  describe command('chef-server-ctl test') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  describe package('opscode-manage') do
+    it { should be_installed }
+  end
+
+  describe command('opscode-manage-ctl test') do
+    its(:exit_status) { should eq 0 }
+  end
+end
